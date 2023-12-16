44f213a084fd77c69a7fa5197031571b39faa7f4
ronangriffin <ronangriffin@192.168.1.72>
2023-12-17 12:05:18 +1300

    Program gives error for non-int text in caesar cipher
    attempted to solve by using try/catch when parsing String to int, but
    unscuccessful.

--- CryptoFX/src/application/Controller.java
+++ CryptoFX/src/application/Controller.java
@@ -16,173 +16,203 @@ import javafx.scene.input.MouseEvent;
 public class Controller implements Initializable {
 	static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 	String textCaesarKey;
-	int caesarKey;
+	static int caesarKey;
 	String caesarInput;
-	
 
-    @FXML
-    private Button buttonDecrypt;
+	@FXML
+	private Button buttonDecrypt;
 
-    @FXML
-    private Button buttonEncrypt;
+	@FXML
+	private Button buttonEncrypt;
 
-    @FXML
-    private TextField enterKey;
+	@FXML
+	private TextField enterKey;
 
-    @FXML
-    private TextField enterText;
+	@FXML
+	private TextField enterText;
 
-    @FXML
-    private Label instructionAES;
+	@FXML
+	private Label instructionAES;
 
-    @FXML
-    private Label instructionCC;
+	@FXML
+	private Label instructionCC;
 
-    @FXML
-    private Label instructionDES;
+	@FXML
+	private Label instructionDES;
 
-    @FXML
-    private RadioButton radioAES;
+	@FXML
+	private RadioButton radioAES;
 
-    @FXML
-    private RadioButton radioCC;
+	@FXML
+	private RadioButton radioCC;
 
-    @FXML
-    private RadioButton radioDES;
+	@FXML
+	private RadioButton radioDES;
 
-    @FXML
-    private TextArea resultText;
-    
-    public static String caesarEncrypt(String caesarInput, String textCaesarKey) {
-    	int caesarKey = Integer.parseInt(textCaesarKey); 
-    	return applyShift(caesarInput, caesarKey);
-    }
-    
-    public static String caesarDecrypt(String caesarEncodeResult, String textCaesarKey) {
-    	int caesarKey = Integer.parseInt(textCaesarKey); 
-    	return applyShift(caesarEncodeResult, 26 - caesarKey);
-    }
-    
-    private static String applyShift(String message, int caesarKey) {
-    	char[] chars = message.toCharArray();
-    	for (int i = 0; i < chars.length; i++) {
-    		char c = chars[i];
-    		if (c >= 'A' && c <= 'Z')
-    			chars[i] = (char) ((c - 'A' +caesarKey) % 26 + 'A');
-    		else if (c >= 'a' && c <= 'z')
-    			chars[i] = (char) ((c - 'a' + caesarKey) % 26 + 'a');
-    	} 	
-    	return new String(chars);
-    }
-    
-    @FXML
-    private void buttonEncrypt(MouseEvent event) throws Exception {
-    	if (enterKey.getText().trim().isEmpty() && enterText.getText().trim().isEmpty()) {
-    		Alert alert = new Alert(Alert.AlertType.ERROR);
-    		alert.setContentText("Text and Key can't be empty");
-    		alert.setHeaderText(null);
-    		alert.setTitle("Encryption Error");
-    		alert.showAndWait();
-    	} else if (enterText.getText().trim().isEmpty()) {
-    		Alert alert = new Alert(Alert.AlertType.ERROR);
-    		alert.setContentText("Text can't be empty");
-    		alert.setHeaderText(null);
-    		alert.setTitle("Encryption Error");
-    		alert.showAndWait();
-    	}  else if (enterKey.getText().trim().isEmpty()) {
-    		Alert alert = new Alert(Alert.AlertType.ERROR);
-    		alert.setContentText("Key can't be empty");
-    		alert.setHeaderText(null);
-    		alert.setTitle("Encryption Error");
-    		alert.showAndWait();
-    	} else {
-    		if (radioCC.isSelected()) {
-    			String caesarInput = enterText.getText().toString();
-    			String textCaesarKey = enterKey.getText().toString();
-    			String caesarCipherText = caesarEncrypt(caesarInput, textCaesarKey).toString();
-    			resultText.setText(caesarCipherText);
-    			//confirming in console
-    			System.out.println("The encrypted message is: " + caesarCipherText);
-    			System.out.println("The decrypted message is: " + (caesarDecrypt(caesarCipherText, textCaesarKey)));
-    		} else if (radioAES.isSelected()) {
-    			
-    		} else if (radioDES.isSelected()) {
-    			
-    		} else {
-        		Alert alert = new Alert(Alert.AlertType.ERROR);
-        		alert.setContentText("Must select an encryption type");
-        		alert.setHeaderText(null);
-        		alert.setTitle("Encryption Error");
-        		alert.showAndWait();
-    		}
-    	}
-    }
-    
-    @FXML
-    private void buttonDecrypt(MouseEvent event) throws Exception {
-    	if (enterKey.getText().trim().isEmpty() && enterText.getText().trim().isEmpty()) {
-    		Alert alert = new Alert(Alert.AlertType.ERROR);
-    		alert.setContentText("Text and Key can't be empty");
-    		alert.setHeaderText(null);
-    		alert.setTitle("Encryption Error");
-    		alert.showAndWait();
-    	} else if (enterText.getText().trim().isEmpty()) {
-    		Alert alert = new Alert(Alert.AlertType.ERROR);
-    		alert.setContentText("Text can't be empty");
-    		alert.setHeaderText(null);
-    		alert.setTitle("Encryption Error");
-    		alert.showAndWait();
-    	}  else if (enterKey.getText().trim().isEmpty()) {
-    		Alert alert = new Alert(Alert.AlertType.ERROR);
-    		alert.setContentText("Key can't be empty");
-    		alert.setHeaderText(null);
-    		alert.setTitle("Encryption Error");
-    		alert.showAndWait();
-    	} else {
-    		if (radioCC.isSelected()) {
-    			
-    			String caesarEncodeResult = enterText.getText().toString();
-    			String textCaesarKey = enterKey.getText().toString();
-    			String caesarCipherPlainText = caesarDecrypt(caesarEncodeResult, textCaesarKey).toString();
-    			resultText.setText(caesarCipherPlainText);
-    			//confirming in console
-    			System.out.println("The decrypted message is: " + caesarCipherPlainText);
-    			System.out.println("The encrypted message is: " + (caesarEncrypt(caesarCipherPlainText, textCaesarKey)));
-    		} else if (radioAES.isSelected()) {
-    			
-    		} else if (radioDES.isSelected()) {
-    			
-    		} else {
-        		Alert alert = new Alert(Alert.AlertType.ERROR);
-        		alert.setContentText("Must select an encryption type");
-        		alert.setHeaderText(null);
-        		alert.setTitle("Encryption Error");
-        		alert.showAndWait();
-    		}
-    	}
-    }
-    
-    @FXML
-    private void radioAES(MouseEvent event) {
-    	radioCC.setSelected(false);
-    	radioDES.setSelected(false);
-    }
+	@FXML
+	private TextArea resultText;
 
-    @FXML
-    private void radioCC(MouseEvent event) {
-    	radioAES.setSelected(false);
-    	radioDES.setSelected(false);
-    }
+	public static String caesarEncrypt(String caesarInput, String textCaesarKey) {
+//		//changing String to int for shift
+//		try {
+//			int caesarKey = Integer.valueOf(textCaesarKey);
+//			System.out.println(caesarKey +" is a valid int");
+//		} catch (NumberFormatException ex) {
+//			ex.printStackTrace();
+//		}
+//		int caesarKey = Integer.valueOf(textCaesarKey);
+		int caesarKey = stringToInt(textCaesarKey);
+		return applyShift(caesarInput, caesarKey);
+	}
 
-    @FXML
-    private void radioDES(MouseEvent event) {
-    	radioAES.setSelected(false);
-    	radioCC.setSelected(false);
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle arg) {
-    	
-    }
+	public static String caesarDecrypt(String caesarEncodeResult, String textCaesarKey) {
+		int caesarKey = Integer.parseInt(textCaesarKey);
+		return applyShift(caesarEncodeResult, 26 - caesarKey);
+	}
+
+	private static String applyShift(String message, int caesarKey) {
+		char[] chars = message.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			if (c >= 'A' && c <= 'Z')
+				chars[i] = (char) ((c - 'A' + caesarKey) % 26 + 'A');
+			else if (c >= 'a' && c <= 'z')
+				chars[i] = (char) ((c - 'a' + caesarKey) % 26 + 'a');
+		}
+		return new String(chars);
+	}
+
+	//attempt to catch non-int strings for caesar ciper but does not work
+	public static int stringToInt(String textCaesarKey) {
+		try {
+			int caesarKey = Integer.parseInt(textCaesarKey);
+			System.out.println(textCaesarKey + " is a valid int");
+		} catch (NumberFormatException e) {
+			System.out.println(textCaesarKey + " is not a valid int");
+		} finally {
+			final int caesarKey = Integer.parseInt(textCaesarKey);
+			return caesarKey;
+		}
+	}
+
+	@FXML
+	private void buttonEncrypt(MouseEvent event) throws Exception {
+		if (enterKey.getText().trim().isEmpty() && enterText.getText().trim().isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("Text and Key can't be empty");
+			alert.setHeaderText(null);
+			alert.setTitle("Encryption Error");
+			alert.showAndWait();
+		} else if (enterText.getText().trim().isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("Text can't be empty");
+			alert.setHeaderText(null);
+			alert.setTitle("Encryption Error");
+			alert.showAndWait();
+		} else if (enterKey.getText().trim().isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("Key can't be empty");
+			alert.setHeaderText(null);
+			alert.setTitle("Encryption Error");
+			alert.showAndWait();
+		} else {
+			if (radioCC.isSelected()) {
+				String caesarInput = enterText.getText().toString();
+				String textCaesarKey = enterKey.getText().toString();
+				// changing String to int for shift
+				try {
+					Integer.parseInt(textCaesarKey);
+					int caesarKey = Integer.parseInt(textCaesarKey);
+					System.out.println(textCaesarKey + " is a valid int");
+				} catch (NumberFormatException e) {
+					System.out.println(textCaesarKey + " is not a valid int");
+				}
+				String caesarCipherText = caesarEncrypt(caesarInput, textCaesarKey).toString();
+				resultText.setText(caesarCipherText);
+				// confirming in console
+				System.out.println("The encrypted message is: " + caesarCipherText);
+				System.out.println("The decrypted message is: " + (caesarDecrypt(caesarCipherText, textCaesarKey)));
+			} else if (radioAES.isSelected()) {
+
+			} else if (radioDES.isSelected()) {
+
+			} else {
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setContentText("Must select an encryption type");
+				alert.setHeaderText(null);
+				alert.setTitle("Encryption Error");
+				alert.showAndWait();
+			}
+		}
+	}
+
+	@FXML
+	private void buttonDecrypt(MouseEvent event) throws Exception {
+		if (enterKey.getText().trim().isEmpty() && enterText.getText().trim().isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("Text and Key can't be empty");
+			alert.setHeaderText(null);
+			alert.setTitle("Encryption Error");
+			alert.showAndWait();
+		} else if (enterText.getText().trim().isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("Text can't be empty");
+			alert.setHeaderText(null);
+			alert.setTitle("Encryption Error");
+			alert.showAndWait();
+		} else if (enterKey.getText().trim().isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("Key can't be empty");
+			alert.setHeaderText(null);
+			alert.setTitle("Encryption Error");
+			alert.showAndWait();
+		} else {
+			if (radioCC.isSelected()) {
+
+				String caesarEncodeResult = enterText.getText().toString();
+				String textCaesarKey = enterKey.getText().toString();
+				int caesarKey = Integer.parseInt(textCaesarKey);
+				String caesarCipherPlainText = caesarDecrypt(caesarEncodeResult, textCaesarKey).toString();
+				resultText.setText(caesarCipherPlainText);
+				// confirming in console
+				System.out.println("The decrypted message is: " + caesarCipherPlainText);
+				System.out
+						.println("The encrypted message is: " + (caesarEncrypt(caesarCipherPlainText, textCaesarKey)));
+			} else if (radioAES.isSelected()) {
+
+			} else if (radioDES.isSelected()) {
+
+			} else {
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setContentText("Must select an encryption type");
+				alert.setHeaderText(null);
+				alert.setTitle("Encryption Error");
+				alert.showAndWait();
+			}
+		}
+	}
+
+	@FXML
+	private void radioAES(MouseEvent event) {
+		radioCC.setSelected(false);
+		radioDES.setSelected(false);
+	}
+
+	@FXML
+	private void radioCC(MouseEvent event) {
+		radioAES.setSelected(false);
+		radioDES.setSelected(false);
+	}
+
+	@FXML
+	private void radioDES(MouseEvent event) {
+		radioAES.setSelected(false);
+		radioCC.setSelected(false);
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle arg) {
+
+	}
 
 }
